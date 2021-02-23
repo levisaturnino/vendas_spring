a8df12f0326d23da0713346a76a7f7593d5e0f24
Levi Saturnino <levisaturnino@gmail.com>
2021-02-23 01:55:42 -0300

    autenticando user

--- src/main/java/br/com/levisaturnino/VendasApplication.java
+++ src/main/java/br/com/levisaturnino/VendasApplication.java
@@ -1,18 +1,7 @@
 package br.com.levisaturnino;
 
-import br.com.levisaturnino.model.entity.Client;
-import br.com.levisaturnino.model.entity.Order;
-import br.com.levisaturnino.model.repository.ClientRepository;
-import br.com.levisaturnino.model.repository.OrderRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.List;
 
 @SpringBootApplication
 public class VendasApplication {
